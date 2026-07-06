f687e1a5563e764daf4ae11fecbf44f56f9cd5f4
BiancaPerasso <bianca.perasso@gmail.com>
2026-07-06 14:33:13 +0200

    remove biased mnist scripts

--- stylegan3/biasedmnist_allrhos.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash 
-
-
-bash biasedmnist_train.sh 90 --kimg 1400 --batch 64
-# bash biasedmnist_train.sh 70 --kimg 600 --batch 64
-# bash biasedmnist_train.sh 80 --kimg 2000 --batch 64
-# bash biasedmnist_train.sh 95 --kimg 2000 --batch 64
-
